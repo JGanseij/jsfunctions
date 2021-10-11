54abf98ee0e3a666e2114243ad26b04ff2b6dc83
JGanseij <88677380+JGanseij@users.noreply.github.com>
2021-10-11 14:48:57 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,36 @@
+
+//Writing or defining a function//
+const makeSpaghetti = function() {
+    console.log("Boil water");
+    console.log("Add pasta.")
+    console.log("Make sauce")
+    console.log("Drain pasta")
+    console.log("Combine pasta and sauce")
+};
+
+ //Using a funciton
+ // The bracelets are very important otherwise it isn't going to execute.
+
+ makeSpaghetti();
+
+ const makePizza = function() {
+     console.log("Roll-out the dough");
+     console.log("Cover the dough with tomatasauce");
+     console.log("Add toppings");
+     console.log("Add the last layer of chees");
+     console.log("Finish the pizza with a bit of olive oil");
+ }
+
+ makePizza()
+
+
+
+const makeSushi = function() {
+    console.log("Get rice");
+    console.log("Get raw fish");
+    console.log("Roll it together");
+}
+
+for (var i = 0; i < 4 ; i++) makeSushi(); 
+
+for (var i = 0; i <3 ; i++ ) makePizza();
